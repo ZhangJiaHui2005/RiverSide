cdaafb4afce2ad85141464fd525dbaa12d8ca48f
thinh204 <huythinh592@gmail.com>
2026-01-29 13:42:23 +0700

    them folder frontend vao du an

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,47 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+import { ClerkProvider } from "@clerk/nextjs";
+import NavBar from "@/components/layout/Navbar";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "RiverSide",
+  description: "Đặt phòng khách sạn theo lựa chọn của bạn",
+  icons: {
+    icon: "/logo.svg",          // favicon chính
+    shortcut: "/logo.svg",      // cho browser
+    apple: "/logo.svg",         // iOS
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en" suppressContentEditableWarning>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <main className="flex flex-col min-h-screen bg-secondary">
+          <NavBar/>
+          <section className="flex-grow">
+            {children}
+          </section>
+        </main>
+      </body>
+    </html>
+    </ClerkProvider>
+  );
+}
